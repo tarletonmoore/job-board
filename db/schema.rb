055509e9c46fe1d0351a658797fe4af199471471
jimmy <jamesmc21998@gmail.com>
2023-08-17 13:59:14 -0500

    create model and controller for jobs

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_17_181513) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_17_185529) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "jobs", force: :cascade do |t|
+    t.integer "company_id"
+    t.string "title"
+    t.string "description"
+    t.string "url"
+    t.string "location"
+    t.boolean "active"
+    t.string "salary_range"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
