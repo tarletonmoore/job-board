9bb0e4641135c19cf50446196161f5b137f496d1
Tarleton Moore <tarletonmoore@Tarletons-MacBook-Pro.local>
2023-08-17 14:22:10 -0400

    add user and sessions controller and authentication

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/login" => "sessions#new"
+  post "/sessions" => "sessions#create"
+  get "/logout" => "sessions#destroy"
+
+  get "/signup" => "users#new"
+  post "/users" => "users#create"
 end
