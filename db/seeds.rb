94504d00cc33e5f91795a2bf77159deb36eb8c46
Tarleton Moore <tarletonmoore@Tarletons-MacBook-Pro.local>
2023-08-17 14:41:18 -0400

    add 2 users to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# user = User.create(name: "John Sense", email: "john@sense.com", password: "password")
+
+# user = User.create(name: "Robert", email: "robert@gmail.com", password: "password")
