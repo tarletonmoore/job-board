b58630496fccb34355f26d21d161306e9ff229b5
Orlando Santos <Santos.Orlando17@gmail.com>
2023-08-18 09:47:21 -0700

    create jobs show route and controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,2 +1,6 @@
 class JobsController < ApplicationController
+  def show
+    @job = Job.find_by(id: params[:id])
+    render :show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
 
   get "/signup" => "users#new"
   post "/users" => "users#create"
+
+  get "/jobs/:id" => "jobs#show"
 end
